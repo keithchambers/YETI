aeafb8b8f29fa2d527832b852f454a9be8c0c7ed
Rajender Gottipamula (AVANADE INC) <v-gotraj@microsoft.com>
2018-12-03 11:55:40 +0530

    Updated application name by order and Removed Unwanted Projects from solution.

--- YammerApplication/YETIDatabase/dbo/Tables/YM_ArchivedThreadsBak.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[YM_ArchivedThreadsBak] (
-    [id]               INT      IDENTITY (1, 1) NOT NULL,
-    [Thread_id]        BIGINT   NULL,
-    [ThreadXMLContent] XML      NULL,
-    [Created_date]     DATETIME NULL,
-    [Modified_date]    DATETIME NULL
-);
-
